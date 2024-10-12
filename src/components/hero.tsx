19032fd3f9fb4dde91036673b195e89d0dbc4288
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2024-10-12 16:32:07 -0600

    Added images to slide show

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -106,14 +106,201 @@ export function Hero() {
 
       <div className="carousel-wrapper w-full max-w-lg mt-6">
         <Slider {...settings}>
+          
           <div>
-            <img src="/assets/image1.jpg" alt="Image 1" className="w-full h-auto" />
+            <img src="/assets/Images_Set1/Picture1.png" alt="set1 Image1" className="w-full h-auto" />
           </div>
+
           <div>
-            <img src="/assets/image2.jpg" alt="Image 2" className="w-full h-auto" />
+            <img src="/assets/Images_Set1/Picture2.png" alt="set1 Image 2" className="w-full h-auto" />
           </div>
+
           <div>
-            <img src="/assets/image3.jpg" alt="Image 3" className="w-full h-auto" />
+            <img src="/assets/Images_Set1/Picture3.jpg" alt="set1 Image 3" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture4.png" alt="set1 Image 4" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture5.jpg" alt="set1 Image 5" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture6.jpg" alt="set1 Image 6" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture8.png" alt="set1 Image 8" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture9.jpg" alt="set1 Image 9" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture10.jpg" alt="set1 Image 10" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture12.jpg" alt="set1 Image 12" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture13.png" alt="set1 Image 13" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture14.png" alt="set1 Image 14" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture15.png" alt="set1 Image 15" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture16.png" alt="set1 Image 16" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture17.png" alt="set1 Image 17" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture18.png" alt="set1 Image 18" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture19.png" alt="set1 Image 19" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture21.png" alt="set1 Image 21" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set1/Picture22.png" alt="set1 Image 22" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set2/Picture1.png" alt="set2 Image 1" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set2/Picture3.png" alt="set2 Image 3" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set2/Picture4.png" alt="set2 Image 4" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set2/Picture6.png" alt="set2 Image 6" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set2/Picture7.png" alt="set2 Image 7" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set2/Picture10.png" alt="set2 Image 10" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set2/Picture11.png" alt="set2 Image 11" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p3.png" alt="set3 Image 3" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p4.png" alt="set3 Image 4" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p5.png" alt="set3 Image 5" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p6.png" alt="set3 Image 6" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p8.png" alt="set3 Image 8" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p9.jpg" alt="set3 Image 9" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p10.jpg" alt="set3 Image 10" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p11.png" alt="set3 Image 11" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p12.png" alt="set3 Image 12" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p13.png" alt="set3 Image 13" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p15.png" alt="set3 Image 15" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p19.png" alt="set3 Image 19" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p20.png" alt="set3 Image 20" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p21.png" alt="set3 Image 21" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p22.png" alt="set3 Image 22" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p23.jpg" alt="set3 Image 23" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p24.png" alt="set3 Image 24" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p25.png" alt="set3 Image 25" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p26.jpg" alt="set3 Image 26" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p28.png" alt="set3 Image 28" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p29.png" alt="set3 Image 29" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p34.png" alt="set3 Image 34" className="w-full h-auto" />
+          </div>
+
+          <div>
+            <img src="/assets/Images_Set3/p37.jpg" alt="set3 Image 37" className="w-full h-auto" />
           </div>
         </Slider>
       </div>
